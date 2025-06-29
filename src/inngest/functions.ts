490bcc6e4051415b2870e840abb93ee9acbb9c82
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-06-30 02:32:45 +0530

    Code generation working with all the tools

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,9 @@
 import Sandbox from "@e2b/code-interpreter";
-import { openai, createAgent } from "@inngest/agent-kit";
+import { openai, createAgent, createTool, createNetwork, Agent } from "@inngest/agent-kit";
 import { inngest } from "./client";
-import { getSandbox } from "./utils";
+import { getSandbox, lastAssistantTextMessageContent } from "./utils";
+import { z } from "zod";
+import { PROMPT } from "@/prompt";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -16,11 +18,127 @@ export const helloWorld = inngest.createFunction(
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "code-agent",
-      system: "You are an expert next js developer You write next js and react snippets.",
-      model: openai({ model: "gpt-4o",apiKey: process.env.OPENAI_API_KEY }),
+      description: "An expert coding agent",
+      system: PROMPT,
+      model: openai({ model: "gpt-4.1",
+        defaultParameters: {
+          temperature: 0.1,
+        },
+        apiKey: process.env.OPENAI_API_KEY }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use terminal to run commands",
+          parameters: z.object({ 
+            command: z.string() 
+          }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = {stdout: "", stderr: ""};
+
+              try{
+                  const sandbox = await getSandbox(sandboxID)
+                  const result = await sandbox.commands.run(command,{
+                  onStdout: (data: string) => {
+                    buffers.stdout += data
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data
+                  }
+                });
+                return result.stdout;
+              }
+              catch(e){
+                console.error(
+                  `Command ${command} failed with error: ${e}\n stdout: ${buffers.stdout}\n stderr: ${buffers.stderr}`
+                );
+                return `Command ${command} failed with error: ${e}\n stdout: ${buffers.stdout}\n stderr: ${buffers.stderr}`
+              }
+            });
+          },
+        }),
+        createTool({
+          name : "createorupdatefiles",
+          description : "Create or update files in the sandbox",
+          parameters : z.object({
+            files:z.array(
+              z.object({
+                path : z.string(),
+                content : z.string(),
+              })
+            )
+          }),
+          handler : async ({ files }, { step , network }) => {
+            const newfiles = await step?.run("createorupdatefiles", async () => {
+              try {
+                const updatedFiles = network.state.data.files || {};
+                const sandbox = await getSandbox(sandboxID);
+                for (const file of files) {
+                  await sandbox.files.write(file.path,file.content);
+                  updatedFiles[file.path] = file.content;
+                }
+                return updatedFiles;
+              } catch (e) {
+                return "Error : " + e;
+              }
+            })
+            if(typeof newfiles == "object"){
+              network.state.data.files = newfiles;
+            }
+          }
+        }),
+        createTool({
+          name : "readfile",
+          description : "Read a file from the sandbox",
+          parameters : z.object({
+            files : z.array(z.string())
+          }),
+          handler : async ({ files }, { step }) => {
+            return await step?.run("readfile", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxID);
+                const contents = [];
+                for (const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({path:file,content:content});
+                }
+                return JSON.stringify(contents);
+              }
+              catch(e){
+                return "Error : " + e;
+              }
+            })
+          }
+        })
+      ],
+      lifecycle: {
+        onResponse: async({result,network}) => {
+          const lastAssistantMessageText = lastAssistantTextMessageContent(result);
+          if(lastAssistantMessageText && network){
+            if(lastAssistantMessageText.includes("<task_summary>")){
+              network.state.data.summary = lastAssistantMessageText;
+            }
+          }
+          return result;
+        },
+      },
     });
 
-    const output = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+    const network = createNetwork({
+      name: "code-agent-network",
+      agents: [codeAgent],
+      maxIter:15,
+      router:async({ network }) => {
+        const summary = network.state.data.summary;
+
+        if(summary){
+          return;
+        }
+        return codeAgent;
+    }
+  });
+    
+    const result = await network.run(event.data.value);
 
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxID)
@@ -28,6 +146,11 @@ export const helloWorld = inngest.createFunction(
       return `https://${host}`
     });
 
-    return { output, sandboxUrl};
+    return {
+      url : sandboxUrl,
+      title : "Fragment",
+      files : result.state.data.files,
+      summary: result.state.data.summary,
+    };
   },
 );
--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,4 +1,5 @@
 import Sandbox from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox (sandboxId : string){
     const sandbox = await Sandbox.connect(sandboxId)
@@ -6,4 +7,15 @@ export async function getSandbox (sandboxId : string){
 };
 
 
- 
+export function lastAssistantTextMessageContent(result : AgentResult){
+    const lastAssistantTextMessageIndex = result.output.findLastIndex(
+        (message) => message.role === "assistant",
+    );
+
+    const message = result.output[lastAssistantTextMessageIndex] as
+    | TextMessage
+    | undefined;
+
+    return message?.content ? typeof message?.content === "string"
+      ? message.content : message?.content.map((c) => c.text).join("") : undefined;
+}
